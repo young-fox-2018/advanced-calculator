946685fe2415d9d93255553296605d99bac6def3
abednegolubis <lubisabednego@gmail.com>
2018-11-05 11:21:29 +0700

    submit challenge advanced calculator

--- calculator.js
+++ calculator.js
@@ -2,19 +2,45 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number) {
+    this.number = number
+    this.phi = 3.14
   }
-  add () {
+  add (number) {
+     this.number +=  number
+     return this
   }
-  subtract () {
+  subtract (number) {
+    this.number -= number
+    return this
   }
-  multiply () {
+  multiply (number) {
+    this.number *= number
+    return this
   }
-  divide () {
+  divide (number) {
+    this.number /= number
+    return this
   }
-  square () {
+  square (number) {
+    this.number = Math.pow(this.number, number)
+    return this
   }
-  squareRoot () {
+  squareRoot (number) {
+    this.number = Number(Math.pow(this.number, 1/number).toFixed(2))
+    return this
+  }
+  circleArea (jariJari) {
+    this.number = Number((this.phi * Math.pow(jariJari,2)).toFixed(2))
+    return this
+  }
+
+  setNumber(number){
+    this.number = number
+    return this
+  }
+  getNumber(){
+    return this.number
   }
 }
 
--- main.js
+++ main.js
@@ -2,4 +2,12 @@
 
 const Calculator = require("./calculator.js")
 
+let kalkulator = new Calculator()
+
+console.log(kalkulator.setNumber(1).add(9).multiply(3).divide(2).subtract(5).getNumber()) //10
+console.log(kalkulator.setNumber(64).squareRoot(2).getNumber()) // 8
+console.log(kalkulator.setNumber(8).square(2).getNumber()) // 64
+console.log(kalkulator.setNumber(8).square(9).squareRoot(3).getNumber()) //512
+console.log(kalkulator.circleArea(4).getNumber()) //50.24
+
 // execute function on calculator.js in here
