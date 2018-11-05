74878dd2dabe6df881dd2f50e02724b3deb64635
Januarsmad <januarsmad@gmail.com>
2018-11-05 15:29:28 +0700

    program done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,37 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number) {
+    this.result = number
+    // this.result = 0
   }
-  add () {
+  add (number) {
+    this.result += number
+    return this
   }
-  subtract () {
+  subtract (number) {
+    this.result -= number
+    return this
   }
-  multiply () {
+  multiply (number) {
+    this.result *= number
+    return this
   }
-  divide () {
+  divide (number) {
+    this.result /= number
+    return this
   }
-  square () {
+  square (number) {
+    this.result = Math.pow(this.input, number)
   }
   squareRoot () {
+    this.result = Math.sqrt(this.result)
+    return this
+  }
+  circle () {
+    const phi = 3.14
+    this.result *= (this.result * this.result * phi)
+    return this
   }
 }
 
--- main.js
+++ main.js
@@ -2,4 +2,10 @@
 
 const Calculator = require("./calculator.js")
 
+let calculate = new Calculator(2)
+
+// calculate.circle(10, 10)
+calculate.multiply(5).circle()
+console.log(calculate.result);
+
 // execute function on calculator.js in here
