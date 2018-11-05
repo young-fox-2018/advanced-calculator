d230ea7827b2cd89d333f81fe7aeedcd247d0dcf
sendyakbar <msendyakbars@gmail.com>
2018-11-05 10:10:37 +0700

    advanced calculator done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,32 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number = 0) {
+    this.result = number
   }
-  add () {
+  add (input) {
+    this.result += input
+    return this
   }
-  subtract () {
+  subtract (input) {
+    this.result -= input
+    return this
   }
-  multiply () {
+  multiply (input) {
+    this.result *= input
+    return this
   }
-  divide () {
+  divide (input) {
+    this.result /= input
+    return this
   }
-  square () {
+  square (input) {
+    this.result = Math.pow(this.result, input)
+    return this
   }
   squareRoot () {
+    this.result = Math.sqrt(this.result)
+    return this
   }
 }
 
@@ -25,5 +38,8 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+let count = new Calculator()
+console.log(count.add(10).subtract(3).multiply(5).divide(7).square(2).squareRoot())
+
 
 module.exports = Calculator
