b72268bc190b7a6f08bc5a4f7829309d96dd326f
Januarsmad <januarsmad@gmail.com>
2018-11-06 15:47:45 +0700

    koreksi phi

--- calculator.js
+++ calculator.js
@@ -3,8 +3,14 @@
 class Calculator {
   //write your code here
   constructor (number) {
-    this.result = number
+    if(number === 'phi'){
+      this.result = Math.PI
+    }else{
+      this.result = number
+    }
+    // this.phi = Math.PI
   }
+
   add (number) {
     this.result += number
     return this
@@ -28,11 +34,15 @@ class Calculator {
     this.result = Math.sqrt(this.result)
     return this
   }
-  circle () {
-    const phi = 22/7
-    this.result *= (this.result * this.result * phi)
+  phi () {
+    this.result = Math.PI
     return this
   }
+  // circle () {
+  //   // const phi = 22/7
+  //   this.result *= (this.result * this.result * phi)
+  //   return this
+  // }
   equal() {
     return this.result
   }
--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 const Calculator = require("./calculator.js")
 
-let calculate = new Calculator(2)
+let calculate = new Calculator('phi')
 
 // calculate.circle(10, 10)
-calculate.multiply(5).circle()
+calculate.multiply(10).multiply(10)
 console.log(calculate.result);
 
 // execute function on calculator.js in here
